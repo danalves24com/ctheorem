2b5caa8e124597a53bed5a25a6f8f450e6bf454d
danalves24com <danalves24@outlook.com>
2022-01-12 06:19:17 +0100

    introduced comments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 
-
 // fn f_prime(x : &f64) -> f64 { 2.0 * x ** &4.0 }
-fn f(x : &f64) -> f64 { x**&5.0 / 2.5  + 10.0 }
-// fn oracle(x : &f64) -> f64 { 0.7 * x }
-fn f_alt(x : &f64) -> f64 { x**&5.0 / 2.5 }
+fn f(x : &f64) -> f64 { x**&5.0 / 2.5  + 10.0 } // original function
+// fn oracle(x : &f64) -> f64 { 0.7 * x } // use a logorithmic fit to predict delta of quotient
+fn f_alt(x : &f64) -> f64 { x**&5.0 / 2.5 } // alternative version to the function f
+// currently missing oracle for constant
+
+fn fit(x : &f64) -> f64 { -0.2647*x.ln() + 0.414}
 
 fn main() {
+
+    // range for shadowing
     let a : f64 = 2.0;
     let b : f64 = 4.0;
+    // counter ...
     let mut count : f64 = 0.0;
 
+
+    // tuple vector of shadows
     let mut yd : Vec<(f64, f64)> = Vec::new();
 
     loop {
@@ -27,9 +34,12 @@ fn main() {
     };
 
     // polynomial fit the quotioent vector
+    // current result:
+    // -0.2647*ln(x) + 0.414
 
 
 
+    // debugging  the data
     let mut prev : f64 = yd[0].1;
     for (x,y) in yd.clone() {
         let change = &prev - y;
